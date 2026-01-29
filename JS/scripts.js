a0d7d7c1a2a37b96c300385049abad890cae0df7
norah-kingsland <norahdawnk@gmail.com>
2026-01-28 22:40:33 -0800

    fix for mobile

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,5 @@
 
 
-// Keeps project title at the right height
-const title = document.querySelector('.project-info-left');
-const image = document.querySelector('.project-media img');
-
-function positionTitle() {
-  const rect = image.getBoundingClientRect();
-
-  title.style.left = `${rect.left + rect.width / 2}px`;
-  title.style.top = `${rect.top + rect.height / 2}px`;
-  title.style.transform = 'translate(-50%, -50%)';
-}
-
-window.addEventListener('resize', positionTitle);
-window.addEventListener('load', positionTitle);
-
-
-
-
-
 
 
 
@@ -46,3 +27,5 @@ document.querySelectorAll('.dropdown').forEach(dropdown => {
     }
   });
 });
+
+
